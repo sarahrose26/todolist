88d70fe2699f05fc2ace96770f3d4c509cd75168
Sarah Dahnke <sarahrosedahnke@gmail.com>
2018-01-17 19:17:55 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var session = require('cookie-session'); // Loads the piece of middleware for sessions
+var bodyParser = require('body-parser'); // Loads the piece of middleware for managing the settings
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+var app = express();
+
+
+/* Using the sessions */
+app.use(session({secret: 'todotopsecret'}))
+
+
+/* If there is no to do list in the session, 
+we create an empty one in the form of an array before continuing */
+.use(function(req, res, next){
+    if (typeof(req.session.todolist) == 'undefined') {
+        req.session.todolist = [];
+    }
+    next();
+})
+
+/* The to do list and the form are displayed */
+.get('/todo', function(req, res) { 
+    res.render('todo.ejs', {todolist: req.session.todolist});
+})
+
+/* Adding an item to the to do list */
+.post('/todo/add/', urlencodedParser, function(req, res) {
+    if (req.body.newtodo != '') {
+        req.session.todolist.push(req.body.newtodo);
+    }
+    res.redirect('/todo');
+})
+
+/* Deletes an item from the to do list */
+.get('/todo/delete/:id', function(req, res) {
+    if (req.params.id != '') {
+        req.session.todolist.splice(req.params.id, 1);
+    }
+    res.redirect('/todo');
+})
+
+/* Redirects to the to do list if the page requested is not found */
+.use(function(req, res, next){
+    res.redirect('/todo');
+})
+
+.listen(8080);   
